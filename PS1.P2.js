56c54e953dd959909209f0896a60af1dd3fb0e8b
Amanda Durfee <amandadurfee@dhcp-wifi-8021x-155-41-88-121.bu.edu>
2020-02-04 18:57:13 -0500

    changed ifs to switch

--- PS1.P2.js
+++ PS1.P2.js
@@ -1,13 +1,19 @@
-const evaluate = term =>
-    { if (term.charAt(1) === '+') {return (left, right) => (parseInt(left) + parseInt(right))}
-    else if (term.charAt(1) === '-') {return (left, right) => (parseInt(left) - parseInt(right))}
-    else if (term.charAt(1) === '%') {return (left, right) => (parseInt(left) % parseInt(right))}
-    else if (term.charAt(1) === '/') {return (left, right) => (parseInt(left) / parseInt(right))}
-    else if (term.charAt(1) === '*') {return (left, right) => (parseInt(left) * parseInt(right))}
+const evaluate = term => {
+    switch (term.charAt(1)) {
+        case '+': return (left, right) => (parseInt(left) + parseInt(right));
+        case '-': return (left, right) => (parseInt(left) - parseInt(right));
+        case '%': return (left, right) => (parseInt(left) % parseInt(right));
+        case '/': return (left, right) => (parseInt(left) / parseInt(right));
+        case '*': return (left, right) => (parseInt(left) * parseInt(right));
     //else (throw "Operation not supported");
-    };
+    }
+};
 
-//const expressions = ['4+2', '5*7', '6-1', '9/2'];
+// const expressions = ['4+2', '5*7', '6-1', '9/2'];
+// const evals = expressions.map(expr => evaluate(expr));
+// and then apply each expression to the list of resulting functions
+
+/* evaluate expression brings back a function that takes two arguments that we then pass it */
 let expression = '4+2';
 console.log(`${expression} = ${evaluate(expression)(expression.charAt((0)), expression.charAt(2))}`);
 
@@ -20,24 +26,3 @@ console.log(`${expression} = ${evaluate(expression)(expression.charAt((0)), expr
 expression = '9/2';
 console.log(`${expression} = ${evaluate(expression)(expression.charAt((0)), expression.charAt(2))}`);
 
-// const operate = evaluate('4+2');
-// console.log(`${operate('4', '2')}`);
-// evaluate('5*7');
-// evaluate('6-1');
-// evaluate('9/2');
-
-// const expr1 = '4+2';
-// const expr2 = '5*7';
-// const expr3 = '6-1';
-// const expr4 = '9/2';
-// const expressions = ['4+2', '5*7', '6-1', '9/2'];
-// /* maybe this should be an array of strings */
-//
-// let result = evaluate(expression);
-// let evaluate = term =>
-//     { if (term.charAt(1) === '+') {(term.charAt(0) + term.charAt(2))}
-//     else if (term.charAt(1) === '-') {(term.charAt(0) - term.charAt(2))}
-//     else if (term.charAt(1) === '%') {(term.charAt(0) % term.charAt(2))}
-//     else if (term.charAt(1) === '/') {(term.charAt(0) / term.charAt(2))}
-//     };
-// expressions.forEach(expr => console.log(expr, '=', ${evaluate(expr)}));
