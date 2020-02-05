cec5c3d1f12a1b940be106eacc620ce0e98915b4
Amanda Durfee <amandadurfee@dhcp-wifi-8021x-155-41-88-121.bu.edu>
2020-02-04 20:03:13 -0500

    capitalA now applies fxn

--- PS1.P3.js
+++ PS1.P3.js
@@ -4,5 +4,29 @@ const superStr = 'supercalifragilisticexpialidocious';
 const splitOnC = appliedFunction(superStr, str => str.split(/(?=c)/));
 console.log(splitOnC);
 
-const capitalA = appliedFunction(superStr, str => str.replace(/a/g, 'A'));
+const capitalA = appliedFunction(superStr, str => { return {
+    'originalString': str,
+    'modifiedString': str.replace(/a/g, 'A'),
+    'numberReplaced': str.split('a').length - 1, //maybe a differences function
+    'length': str.length
+    }});
 console.log(capitalA);
+
+// V. 01
+// const capitalA =
+//     {
+//         'originalString': superStr,
+//         'modifiedString': appliedFunction(superStr, str => str.replace(/a/g, 'A')),
+//         'numberReplaced': superStr.split('a').length - 1,
+//         'length': superStr.length
+//     };
+
+// V. 02
+
+// const capitalA = appliedFunction(appliedFunction(superStr, str => str.replace(/a/g, 'A')), str => { return
+//     {
+//     'originalString': superStr,
+//     'modifiedString': str,
+//     'numberReplaced': superStr.split('a').length - 1, //maybe a differences function
+//     'length': superStr.length
+//     }});
