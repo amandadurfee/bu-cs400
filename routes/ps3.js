e5d9d181e59d30db72d8061db81d748111d56929
Amanda Durfee <amandadurfee@dhcp-wifi-8021x-155-41-10-238.bu.edu>
2020-02-27 17:02:22 -0500

    changed to vars

--- routes/ps3.js
+++ routes/ps3.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET constant string */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'swiggity swooty. i want the booty'});
+    res.render('index', { title: 'swiggity swooty. i want the booty' });
 });
 
 /* POST constant string */
 router.post('/', function(req, res, next) {
-    res.render('post', { string: req.body.string, stringLen: req.body.string.length});
+    res.render('post', { string: req.body.string, stringLen: req.body.string.length });
 });
 
 module.exports = router;
