dd55471af7576e6aaf5f95adbcc6de97f2b276b9
amandadurfee <adurfee@bu.edu>
2020-04-29 21:51:01 -0400

    ensured invalid codes wouldn't get called

--- src/app/validated-form/validated-form.component.ts
+++ src/app/validated-form/validated-form.component.ts
@@ -13,18 +13,24 @@ export class ValidatedFormComponent implements OnInit {
   countryCode: FormControl = new FormControl('');
   selectedCountry: string;
   results: any;
+  validCountry = true;
   ngOnInit(): void {
   }
   getStats() {
     console.log('Validated-form getStats() called.');
     this.selectedCountry = this.countryCode.value;
-    this.cvd.getData(this.selectedCountry).subscribe(
-      response => {
-        console.log('subscribed!');
-        console.log(response);
-        this.results = response;
-      }
-    );
+    console.log(this.selectedCountry);
+    if (['USA', 'CHN', 'ITA'].includes(this.selectedCountry)) {
+      this.cvd.getData(this.selectedCountry).subscribe(
+        response => {
+          console.log('subscribed!');
+          console.log(response);
+          this.results = response;
+        }
+      );
+    } else {
+      this.validCountry = false;
+    }
   }
 
   constructor(private cvd: CovidService) { }
